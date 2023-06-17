4804751a352489993627464eeac2183053e8dfa1
Rupam Banerjee <rupam.banerje@gmail.com>
2023-06-17 22:53:15 +0530

    redirecting link added

--- script.js
+++ script.js
@@ -8,10 +8,48 @@ Signinbtn.onclick = function(){
     titleField.innerHTML = "Sign In"
     Signinbtn.classList.remove('disabled')
     Signupbtn.classList.add('disabled')
+
+    const email=document.getElementById("enter-email").value;
+    const password=document.getElementById("enter-password").value;
+
+    if (email == "") {
+        return;
+    }
+    const getData = JSON.parse(localStorage.getItem('user'));
+     
+
+    if(getData.email === email && getData.password === password ){
+        
+
+        window.location.assign("https://subhra-saree-website.netlify.app/");
+
+    }
+
 }
+
+
 Signupbtn.onclick = function(){
     nameField.style.maxHeight = "65px";
     titleField.innerHTML = "Sign Up"
     Signinbtn.classList.add('disabled')
     Signupbtn.classList.remove('disabled')
+
+    const username=document.getElementById("enter-user").value;
+    const email=document.getElementById("enter-email").value;
+    const password=document.getElementById("enter-password").value;
+
+    const user ={
+        username: username,
+        email:email,
+        password:password
+    }
+
+    
+localStorage.setItem('user' , JSON.stringify(user));
+alert('Sign Up succesfull');
+window.location.reload();
+
+    
 }
+
+
